681f6c75b7942b50db42b6d6b5f8b381ac5a1418
lutful bari <lutfulbarimusa@gmail.com>
2023-01-20 07:16:40 +0600

    TextToSpeach

---
+++ app/src/main/java/com/lutful/texttovoice/MainActivity.java
@@ -0,0 +1,46 @@
+package com.lutful.texttovoice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            if (editText.getText().toString().length()>0){
+                textToSpeech.speak(""+editText.getText().toString(), TextToSpeech.QUEUE_FLUSH,null, null);
+
+            }else {
+                editText.setError("wright something");
+            }
+
+            }
+        });
+
+    }
+}
